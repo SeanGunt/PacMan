75230789e17a85f59f82deaa959011a2d3bcbcc7
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-01-27 13:18:46 -0500

    Code Changes

--- Assets/Scripts/PacManController.cs
+++ Assets/Scripts/PacManController.cs
@@ -6,52 +6,50 @@ using UnityEngine.UI;
 public class PacManController : MonoBehaviour
 {   
     public float speedMultiplier;
-
-    Rigidbody2D rigidbody2d;
     private Vector2 direction;
     public Text livesText;
-    public Text winText;
-
-    private int lives;
-
+    public Text gameOverText;
+    public Text scoreText;
+    private int lives = 3;
+    private int score = 0;
+    Rigidbody2D rigidbody2d;
     Animator animator;
     AudioSource audioSource;
 
-    private void Start()
+    private void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
-        livesText.text = "";
-        winText.text = "";
-
-        lives = 3;
-
         audioSource = GetComponent<AudioSource>();
+
+        livesText.text = "Lives: " + lives.ToString();
+        scoreText.text = score.ToString();
+        gameOverText.text = "";
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             direction = Vector2.up;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S)|| Input.GetKeyDown(KeyCode.DownArrow))
         {
             direction = Vector2.down;
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.RightArrow))
         {
             direction = Vector2.right;
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.LeftArrow))
         {
             direction = Vector2.left;
         }
     }
     private void FixedUpdate()
     {
-        transform.position = new Vector2(transform.position.x + direction.x * speedMultiplier, transform.position.y + direction.y * speedMultiplier);
+        transform.position = new Vector2(transform.position.x + direction.x * Time.deltaTime * speedMultiplier , 
+        transform.position.y + direction.y * Time.deltaTime * speedMultiplier);
     }  
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -59,17 +57,22 @@ public class PacManController : MonoBehaviour
         if (collision.collider.tag == "Enemy")
         {
             lives = lives - 1;
+            livesText.text = "Lives: " + lives.ToString();
         }
-
-         livesText.text = "Lives: " + lives.ToString();
-        if (lives <=0)
+        if (lives <= 0)
         {
-            winText.text = "You lose!";
+            gameOverText.text = "You lose!";
+            Destroy(this.gameObject);
         }
+    }
 
-        if (lives == 0)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Dot")
         {
-            Destroy(gameObject);
+            score += 10;
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
         }
     }
 }
